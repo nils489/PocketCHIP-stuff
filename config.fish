5d4f3f6f5efcf8ca635373e45c3ede40ae95140f
Nils Kornfeld <LNelson@gmx.de>
2017-05-20 16:34:34 +0200

    added config.fish

---
+++ config.fish
@@ -0,0 +1,5 @@
+alias telegram "/home/chip/tg/bin/telegram-cli -k /home/chip/tg/tg-server.pub"
+alias btbx_init "pactl set-default-sink bluez_sink.40_EF_4C_A0_23_A0"
+alias conv_pc "/home/chip/scripts/convert.sh"
+set PATH /home/chip/jdk1.8.0_121/bin $PATH
+set JAVA_HOME /home/chip/jdk1.8.0/bin/java
