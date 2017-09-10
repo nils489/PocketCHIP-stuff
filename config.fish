99126eb2232bf492de264684ba68e9476db6ca84
Nils Kornfeld <LNelson@gmx.de>
2017-09-10 22:18:29 +0200

    changed marshall_init to set volume to 52 %

--- config.fish
+++ config.fish
@@ -1,6 +1,8 @@
 alias telegram "/home/chip/tg/bin/telegram-cli -k /home/chip/tg/tg-server.pub"
-alias btbx_init "pactl set-default-sink bluez_sink.40_EF_4C_A0_23_A0"
-alias marshall_init "pactl set-default-sink bluez_sink.5C_EB_68_4D_4E_CB"
+alias btbx_init "pactl set-default-sink bluez_sink.00_00_00_00_00_00"
+alias marshall_init "bash /home/chip/scripts/marsh.sh"
+alias medion_init "pactl set-default-sink bluez_sink.00_00_00_00_00_00"
 alias conv_pc "/home/chip/scripts/convert.sh"
+alias startvnc "/home/chip/scripts/startvnc.sh"
 set PATH /home/chip/jdk1.8.0_121/bin $PATH
 set JAVA_HOME /home/chip/jdk1.8.0/bin/java
---
+++ scripts/marsh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pactl set-default-sink bluez_sink.00_00_00_00_00_00
+amixer set Master 52%
